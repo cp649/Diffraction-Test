628143e6bb77a1560382fbeaeeb6faa22b1e52e1
Bilbo Baggins <jsb353drexel@gmail.com>
2017-05-12 09:38:36 -0400

    Revert "MacBookAirCode"
    This reverts commit 90444b5e2797f2172e70ad1bb81dcae768a70c3a.

--- Generate_Intensity_2theta.m
+++ Generate_Intensity_2theta.m
@@ -4,18 +4,15 @@
 % diffraction geometry of the (102) peak (noncoplanar)
 % It needs the Lattice and Probe to get information for StructureFactor,
 % Threshold to limit the peaks to only the important ones (it is
-% intensity), Range to define how close two peaks can be together before
+% intensity), Resolution to define how close two peaks can be together before
 % they are interpreted as a single one from the same family of planes and
-% span/IndexMax for what Miller indeces should be analyzed 
-% e.g. span=1 100 010 001 110 101 110 111 
-% e.g. span=2 100 010 001 110 101 110 111 102 120 201 210 012 021 and so
-% on.
+% span/IndexMax for what planes should be analyzed 
 % 
 %
 %
-% Last updated 4-12-2017 Cosmin Popescu
+% Last updated 5-8-2017 Cosmin Popescu
 
-% function [PlotIntensity,Lattice, Probe]=Generate_Intensity_2theta(Lattice, Probe,Threshold, Range, IndexMax,FigNum)
+function Table=Generate_Intensity_2theta(Lattice, Probe,Threshold, Resolution, hkl,FigNum)
 %% Get the XRD Pattern
 % Run through StructureFactor with the same parameters except the
 % Lattice.Normal to obtain multiple 2theta angles.
@@ -27,30 +24,40 @@ addpath(genpath('C:\Users\Cosmin\Desktop\Cr2AlC'))
 addpath(genpath('C:\Users\Cosmin\Desktop\Diffraction-master\StructureLibrary'))
 addpath(genpath('C:\Users\Cosmin\Desktop\Diffraction-master\TestScripts'))
 %addpath(genpath('C:\Users\Cosmin\Desktop\Diffraction-master'))
-%clear;
-% Load your material
-%load graphite.mat
 
-% DEFINE YOUR X-RAYS
-%Probe.Type = 'x-ray';
-%Probe.Energy = 8047; % [eV] % define either Energy or lambda
-Probe.Polarization = 's'; % s (perpendicular) or p (parallel)
-
-Threshold=0.001;
-Range=0.1;
-IndexMax=9;
-FigNum=[];
+% % Load your material
+% load Cr2AlC.mat
+% 
+% % DEFINE YOUR X-RAYS
+% Probe.Type = 'x-ray';
+% Probe.Energy = 8047; % [eV] % define either Energy or lambda
+% Probe.Polarization = 's'; % s (perpendicular) or p (parallel)
+% 
+% Threshold=1;
+% Resolution=0.1;
+% IndexMax=9;
+% FigNum=[];
 
 %% Loop over different hkl values individually. 
 % There will be repetitions
 % like 001 and 002
+% The software goes through all combinations given by user of Miller
+% indeces and returns the resulting information from Structure Factor. This
+% information is saved into DataOfIntensity which is used as file of raw
+% data. It contains among other things: complex values for the intensity
+% and the Bragg angle. This are removed further in the script.
+% DataOfIntensity is the first storage point/variable but the main
+% operations are done with PlotIntensity which is a matrix type variable as
+% well.
+TypeOfFile=input('What kind of file extension do you want?\nOptions: nothing (press Enter), txt, xlsx, xls, dat, csv \n','s');
+
 DataOfIntensity=zeros(10,4);
 countofdata=1;
 
 
-for h=0:IndexMax
-    for k=0:IndexMax
-        for l=0:IndexMax
+for h=-hkl:hkl
+    for k=-hkl:hkl
+        for l=-hkl:hkl
             if h==0 && k==0 && l==0
                 % This is to eliminate the 000
             else
@@ -68,6 +75,8 @@ for h=0:IndexMax
 end
 
 %% Eliminate the values with intensity below a threshold.
+% PlotIntensity is created by going through all raw data and removing all
+% the BraggAn
 countofplot=1;
 PlotIntensity=zeros(1,5);
 for i=1:countofdata-1
@@ -98,21 +107,21 @@ end
     
 
 %% Make Plot
-if isempty(FigNum)
-    figure
-else
+
+if nargin>5
     figure(FigNum)
+    
+    plot(PlotIntensity(:,5),PlotIntensity(:,4),'ob','linewidth',2);
+    xlabel('2\theta');
+    ylabel('Intensity');
+    Title=strcat(Lattice.Symbol,'-',Lattice.Type);
+    title(Title);
+    axis([0 180 0 1.1*max(PlotIntensity(:,4))]);
+    hold on;
 end
-plot(PlotIntensity(:,5),PlotIntensity(:,4),'ob','linewidth',2);
-xlabel('2\theta');
-ylabel('Intensity');
-Title=strcat(Lattice.Symbol,'-',Lattice.Type);
-title(Title);
-axis([0 180 0 1.1*max(PlotIntensity(:,4))]);
-hold on;
 
 %% Add the Miller indeces to the plot
-xseparation=2;
+xseparation=-2;
 yseparation=max(PlotIntensity(:,4))*0.04;
 % This is a way to separate multiple combinations of hkl that give the same
 % theta. 
@@ -124,12 +133,13 @@ for i=2:countofplot-1
 end
 % Write text at a separation from the point. If multiple point in the same
 % location, stack the hkl in order defined by previous for.
-for i=1:countofplot-1
-    h=PlotIntensity(i,1);
-    k=PlotIntensity(i,2);
-    l=PlotIntensity(i,3);
-    text( PlotIntensity(i,5)+xseparation,PlotIntensity(i,4)+yseparation*PlotIntensity(i,6),strcat([num2str(h), num2str(k), num2str(l)]))
-
+if nargin >5
+    for i=1:countofplot-1
+        h=PlotIntensity(i,1);
+        k=PlotIntensity(i,2);
+        l=PlotIntensity(i,3);
+        text( PlotIntensity(i,5)+xseparation,PlotIntensity(i,4)+yseparation*PlotIntensity(i,6),strcat([num2str(h), num2str(k), num2str(l)]))
+    end
 end
 %% Intensities on the same angle are added up
 
@@ -141,7 +151,7 @@ for i=1:length
     currentposition=i;
     if(n<length-1)
         while(PlotIntensity(n+1,6)>1)
-            PlotIntensity(currentposition,8)=PlotIntensity(currentposition,8);%+PlotIntensity(n+1,8);
+            PlotIntensity(currentposition,8)=PlotIntensity(currentposition,8)+PlotIntensity(n+1,8);
             PlotIntensity(n+1,8)=0;
             if(n+1<length)
             n=n+1;
@@ -158,21 +168,27 @@ for i=1:length
 end
 
 %% Generating XRD plot with resolution based on range.
-if isempty(FigNum)
-    figure
-else
+if nargin>5
     figure(FigNum+1)
+else
+    figure
 end
 countAngle=1;
-XRD_plot=zeros(ceil(180/Range+length),5);
+XRD_plot=zeros(ceil(180/Resolution+length),5);
 indexPlot=1;
-for angle=0:Range:180
-    if (angle<PlotIntensity(countAngle,5))&& (angle+Range>PlotIntensity(countAngle,5)&&countAngle<length-1)
-        XRD_plot(indexPlot,1)=PlotIntensity(countAngle,1); %h
-        XRD_plot(indexPlot,2)=PlotIntensity(countAngle,2); %k
-        XRD_plot(indexPlot,3)=PlotIntensity(countAngle,3); %l
-        XRD_plot(indexPlot,4)=PlotIntensity(countAngle,5); %2theta
-        XRD_plot(indexPlot,5)=PlotIntensity(countAngle,8); %Intensity of the actual point
+index=1;
+
+for angle=0:Resolution:180
+    if (angle<PlotIntensity(countAngle,5))&& (angle+Resolution>PlotIntensity(countAngle,5)&&countAngle<length-1)
+        Table.h=PlotIntensity(countAngle,1); %h
+        Table.k=PlotIntensity(countAngle,2); %k
+        Table.l=PlotIntensity(countAngle,3); %l
+        Table.TwoTheta=PlotIntensity(countAngle,5);
+        Table.Intensity=PlotIntensity(countAngle,8); %Piggyback on data processing.
+        Array(index)=Table;
+        index=index+1;
+        XRD_plot(indexPlot,1)=PlotIntensity(countAngle,5); %2theta
+        XRD_plot(indexPlot,2)=PlotIntensity(countAngle,8); %Intensity of the actual point
         indexPlot=indexPlot+1;
         if countAngle<length-1
             while(PlotIntensity(countAngle+1,6)>1&&(countAngle<(length-2)))
@@ -181,29 +197,31 @@ for angle=0:Range:180
         end
         countAngle=countAngle+1;
     else
-        XRD_plot(indexPlot,4)=angle; %Looping through the angles.
-        XRD_plot(indexPlot,5)=0; %No intensity
+        XRD_plot(indexPlot,1)=angle; %Looping through the angles.
+        XRD_plot(indexPlot,2)=0; %No intensity
         indexPlot=indexPlot+1;
         
     end       
 end
-plot(XRD_plot(:,4),XRD_plot(:,5),'-b');
-xlabel('2\theta');
-ylabel('Intensity');
-Title=strcat(Lattice.Symbol,'-',Lattice.Type);
-title(Title);
-axis([0 180 0 1.1*max(XRD_plot(:,5))]);
+% subplot(2,1,1);
 hold on;
-maxx=max(XRD_plot(:,5));
-XRD_plot(:,5) = 100*XRD_plot(:,5)/maxx;
-
+plot(XRD_plot(:,1),XRD_plot(:,2)*100/max(XRD_plot(:,2)),'-b','linewidth',1.5);
+xlabel('2\theta');
+ylabel('Intensity percent I/I_M_a_x');
+Title=strcat(Lattice.Symbol,' - ',Lattice.Type,' 8048.3 eV');
+title(Title);
+axis([0 180 0 105]);
+hold on;
+maximum=max(XRD_plot(:,2));
+PlotIntensity(:,9)=100*PlotIntensity(:,8)/maximum;
+legend('MATLAB');
 %% A new plot similar to actual XRD is generated.
 % Xtheta=zeros(5,180+length);
 % count=1;
 % % Make 0 in intensity everything that is not in the data.
 % for i=1:180+length
 %     if count<=length
-%         if((PlotIntensity(count,5)-i+count)<Range)
+%         if((PlotIntensity(count,5)-i+count)<Resolution)
 %             Xtheta(1,i)=PlotIntensity(count,5);
 %             Xtheta(2,i)=PlotIntensity(count,8);
 %             Xtheta(3,i)=PlotIntensity(count,1);
@@ -227,24 +245,56 @@ XRD_plot(:,5) = 100*XRD_plot(:,5)/maxx;
 % axis([0 180 0 1.1*max(Xtheta(:,2))]);
 % hold on;
 %% Make name for the text file
-
-    Filename=strcat(Lattice.Symbol,'_',Lattice.Type,'.txt');
-
+% Dealt with this below. See comment after section below.
+% Mostly old code here.
+%     Filename=strcat(Lattice.Symbol,'_',Lattice.Type,'.txt');
+%     MAX=max(PlotIntensity(:,8));
+%     PlotIntensity(:,8)=PlotIntensity(:,8)/MAX;
 
 %% Make table with Miller indeces.
-fid=fopen(Filename,'a');
-fprintf(fid,'\r\n');
-fprintf(fid,'Start of a new data set \r\n');
-fprintf(fid,strcat('The energy is ',num2str(Probe.Energy)));
-fprintf(fid,'\r\n %s %s %s %s %s\r\n','h','k','l','2theta','Intensity');
-for i=1:length
-    if(PlotIntensity(i,6)==1)
-        fprintf (fid,'%d %d %d %f %f\r\n',PlotIntensity(i,1),PlotIntensity(i,2),PlotIntensity(i,3),PlotIntensity(i,5),PlotIntensity(i,8));
-        
-%     else
-%         fprintf(fid,'%d %d %d\r\n',PlotIntensity(i,1),PlotIntensity(i,2),PlotIntensity(i,3));
-        
+Table=struct2table(Array);
+time=datestr(datetime('now'));
+time(15)='_';
+time(18)='_';
+time(12)='_';
+NameOFile=strcat(Lattice.Symbol,'_',Lattice.Type,'_',num2str(Probe.Energy),'eV','_',time,'.');
+
+if size(TypeOfFile,2)==4
+    if TypeOfFile=='xlsx'
+        writetable(Table,strcat(NameOFile,'xlsx'));
     end
+elseif size(TypeOfFile,2)==3
+    if TypeOfFile== 'txt'
+        writetable(Table,strcat(NameOFile,'txt'));
+    elseif TypeOfFile=='dat'
+        writetable(Table,strcat(NameOFile,'dat'));
+    elseif TypeOfFile == 'csv'
+        writetable(Table,strcat(NameOFile,'csv'));
+    elseif TypeOfFile == 'xls'
+        writetable(Table,strcat(NameOFile,'xls'));
+    end
+else %default do nothing
 end
-fclose(fid);
+
+
+
+
+% Former text file generation on the basis of the data generated. Updated
+% version of data output above in table generation with different
+% extension.
+% fid=fopen(Filename,'a');
+% fprintf(fid,'\r\n');
+% fprintf(fid,'Start of a new data set \r\n');
+% fprintf(fid,strcat('The energy is  ',num2str(Probe.Energy)));
+% fprintf(fid,'\r\n %s %s %s %s %s\r\n','h','k','l','2theta','Intensity');
+% for i=1:length
+%     if(PlotIntensity(i,6)==1)
+%         fprintf (fid,'%d %d %d %f %f\r\n',PlotIntensity(i,1),PlotIntensity(i,2),PlotIntensity(i,3),PlotIntensity(i,5),PlotIntensity(i,8));
+%         
+% %     else
+% %         fprintf(fid,'%d %d %d\r\n',PlotIntensity(i,1),PlotIntensity(i,2),PlotIntensity(i,3));
+%         
+%     end
+% end
+% fclose(fid);
 
--- Graphite_GeometricalSimulation_noncoplanar.m
+++ Graphite_GeometricalSimulation_noncoplanar.m
@@ -2,14 +2,17 @@
 
 % CLEAN WORKSPACE
 home
-clear
+%clear
 %close all
 
 % PATH TO DIFFWIZ LIBRARY
 addpath(genpath('C:\Users\maher\Google drive\DIFFWIZ\'))
+addpath(genpath('C:\Users\Cosmin\Desktop\Grand Diffraction Master\'))
 
 % DEFINE LATTICE OR LOAD LATTICE FROM EXISTING STRUCTURE LIBRARY
+addpath(genpath('C:\Users\Cosmin\Desktop\Cr2AlC'))
 load graphite.mat
+%load graphite.mat
 
 % DEFINE DIRECTION OF CRYSTAL NORMAL
 Lattice.Normal = [0 0 1]; 
@@ -18,18 +21,25 @@ Lattice.Normal = [0 0 1];
 Probe.Type = 'xrays';
 Probe.Energy = 18000; % [eV]
 Probe.DiffractionGeometry = 'noncoplanar';
-Probe.psi = 0.1; % This's the grazing angle
+Probe.psi = 0.2; % This's the grazing angle
 
 % DEFINE DETECTOR
 Detector.Shape = 'square';
-Detector.Size = 102; % diameter or length in mm
-Detector.SpotFWHMx = 3; % Diffraction spot size on detector
-Detector.SpotFWHMy = 3;
-Detector.DistanceToSample = 100; % Sample-detector distance in mm
-Detector.Offset = [0 40]; % offset from center of beam in mm
+Detector.Size = 50; % diameter or length in mm
+Detector.SpotFWHMx = 2; % Diffraction spot size on detector
+Detector.SpotFWHMy = 2;
+Detector.DistanceToSample = 50; % Sample-detector distance in mm
+Detector.Offset = [0 25]; % offset from center of beam in mm
 
 % MILLER INDICES TO LOOP OVER 
-hkl = [0:9];
+hkl = 0:10;
 
 % MAIN FUNCTION
-[I SF] = GeometricalSimulation1(Lattice, Probe, Detector, hkl, 1);
+I = GeometricalSimulation1(Lattice, Probe, Detector, hkl, 1);
+
+
+
+
+
+
+
